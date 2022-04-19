264e9bc5d4cd9fe27cb5d12fd5c08c5483f79858
陈彦旎 <annechen@chenyannidembp.myfiosgateway.com>
2022-04-19 13:36:06 -0400

    debugging

--- src/app.js
+++ src/app.js
@@ -20,15 +20,73 @@ app.set('view engine', 'hbs');
 const Review = mongoose.model('Review');
 
 app.get('/api/reviews', function(req, res) {
-  // TODO: retrieve all reviews or use filters coming in from req.query
-  // send back as JSON list
+    // TODO: retrieve all reviews or use filters coming in from req.query
+    // send back as JSON list
+    if (req.query.year.length > 0 || req.query.semester.length > 0) {
+        if (req.query.year.length > 0 && req.query.semester.length > 0) {
+            const temp = Review.find({ year: { $eq: req.query.year } });
+            const reviews = temp.find({ semester: { $eq: req.query.semester } });
+            res.json(reviews.map(function(ele) {
+                return {
+                    'name': ele.name,
+                    'semester': ele.semester,
+                    'year': ele.year,
+                    'review': ele.review
+                };
+            }));
+        } else if (req.query.year.length > 0) {
+            const temp = Review.find({ year: { $eq: req.query.year } });
+            res.json(temp.map(function(ele) {
+                return {
+                    'name': ele.name,
+                    'semester': ele.semester,
+                    'year': ele.year,
+                    'review': ele.review
+                };
+            }));
+        } else {
+            const reviews = Review.find({ semester: { $eq: req.query.semester } });
+            res.json(reviews.map(function(ele) {
+                return {
+                    'name': ele.name,
+                    'semester': ele.semester,
+                    'year': ele.year,
+                    'review': ele.review
+                };
+            }));
+        }
+    } else {
+        Review.find({}, function(err, m) {
+            res.json(m.map(function(ele) {
+                return {
+                    'name': ele.name,
+                    'semester': ele.semester,
+                    'year': ele.year,
+                    'review': ele.review
+                };
+            }));
+        });
+    }
 });
 
 app.post('/api/review/create', (req, res) => {
-  // TODO: create new review... if save succeeds, send back JSON
-  // representation of saved object
+    // TODO: create new review... if save succeeds, send back JSON
+    // representation of saved object
+    (new Review({
+        name: req.body.name,
+        semester: req.body.semester,
+        year: req.body.year,
+        review: req.body.review
+    })).save((err, review) => {
+        if (err) {
+            console.log(err);
+            res.json(err);
+        } else {
+            res.json(review);
+        }
+    });
 });
 
 app.listen(process.env.PORT || DEFAULT_AIT_PORT, (err) => {
-  console.log('Server started (ctrl + c to shut down)');
+    console.log('Server started (ctrl + c to shut down)');
 });
--- src/db.js
+++ src/db.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 
 //using mongoose models
 const ReviewSchema = mongoose.Schema({
-  name: String,
-  semester: String,
-  year: String,
-  professor: String,
-  review: String,
+    name: String,
+    semester: String,
+    year: String,
+    professor: String,
+    review: String,
 });
 
 mongoose.model('Review', ReviewSchema);
 
 mongoose.connect('mongodb://localhost/hw07', (err, database) => {
-  if (err) {
-    return console.log(err);
-  } else {
-    console.log('Connected to database'); 
-  }
+    if (err) {
+        return console.log(err);
+    } else {
+        console.log('Connected to database');
+    }
 });
