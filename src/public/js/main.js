a6ed8dc9da54a2570d26a6e60defb48455a65c0f
陈彦旎 <annechen@chenyannideMBP.attlocal.net>
2022-04-21 00:33:37 -0700

    changing fetch

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,75 +1,8 @@
 // TODO: add client side code for single page application
 function getReviews() {
-    const req = new XMLHttpRequest();
     const url = 'http://localhost:3000/api/reviews';
-    req.open('GET', url, true);
-    req.addEventListener('load', function handleReviews() {
-        if (req.status >= 200 && req.status < 400) {
-            const reviewList = document.getElementById('reviews-list');
-            reviewList.innerHTML = '';
-            const reviews = JSON.parse(req.responseText);
-            reviews.forEach((m) => {
-                const tr = document.createElement('tr');
-                const td = document.createElement('td');
-                tr.appendChild(td).textContent = m.name;
-                const td2 = document.createElement('td');
-                tr.appendChild(td2).textContent = m.semester;
-                const td3 = document.createElement('td');
-                tr.appendChild(td3).textContent = m.year;
-                const td4 = document.createElement('td');
-                tr.appendChild(td4).textContent = m.review;
-                if (m.name !== null && m.year !== null && m.semester !== null && m.review !== null) {
-                    reviewList.appendChild(tr);
-                }
-            });
-        }
-    });
-    req.addEventListener('error', function() {
-        console.log("ERROR");
-    });
-    req.send();
-}
-
-function add(evt) {
-    evt.preventDefault();
-    const req = new XMLHttpRequest();
-    const url = 'http://localhost:3000/api/review/create';
-    req.open('POST', url, true);
-    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-    const semester = document.getElementById('semester').value;
-    const name = document.getElementById('name').value;
-    const year = document.getElementById('year').value;
-    const review = document.getElementById('review').value;
-    const data = `name=${name}&semester=${semester}&year=${year}&review=${review}`;
-    req.send(data);
-    req.addEventListener('load', function() {
-        getReviews();
-    });
-}
-
-function filter(evt) {
-    evt.preventDefault();
-    const req = new XMLHttpRequest();
-    let url = 'http://localhost:3000/api/reviews';
-    const year = document.getElementById('filterYear').value;
-    const semester = document.getElementById('filterSemester').value;
-    console.log(year + "FILTERREVIEWS");
-    if (year.length > 0 && semester !== "") {
-        console.log(url, "URL");
-        url += '?year=' + year + '&semester=' + semester;
-    } else if (semester !== "") {
-        console.log(url, "URL");
-        url += '?semester=' + semester;
-    } else if (year.length > 0) {
-        console.log(url, "URL");
-        url += '?year=' + year;
-    }
-    console.log(url, "AFTER");
-    req.open('GET', url, true);
-    req.addEventListener('load', function handleFilter() {
-        if (req.status >= 200 && req.status < 400) {
-            console.log("RESPONSE TEXT ", req.responseText);
-            const data = JSON.parse(req.responseText);
+    fetch(url).then(function(response) { return response.json(); })
+        .then(function(data) {
             const reviewList = document.getElementById('reviews-list');
             reviewList.innerHTML = '';
             data.forEach((m) => {
@@ -86,9 +19,68 @@ function filter(evt) {
                     reviewList.appendChild(tr);
                 }
             });
+        });
+}
+
+function add(evt) {
+    evt.preventDefault();
+    const url = 'http://localhost:3000/api/review/create';
+    const semester = document.getElementById('semester').value;
+    const name = document.getElementById('name').value;
+    const year = document.getElementById('year').value;
+    const review = document.getElementById('review').value;
+    const data = `name=${name}&semester=${semester}&year=${year}&review=${review}`;
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: data
+    }).then(function(response) {
+        if (response.status >= 200 && response.status < 400) {
+            getReviews();
         }
     });
-    req.send();
+
+}
+
+function filter(evt) {
+    evt.preventDefault();
+    let url = 'http://localhost:3000/api/reviews';
+    const year = document.getElementById('filterYear').value;
+    const semester = document.getElementById('filterSemester').value;
+    console.log(year + "FILTERREVIEWS");
+    if (year.length > 0 && semester !== "") {
+        console.log(url, "URL");
+        url += '?year=' + year + '&semester=' + semester;
+    } else if (semester !== "") {
+        console.log(url, "URL");
+        url += '?semester=' + semester;
+    } else if (year.length > 0) {
+        console.log(url, "URL");
+        url += '?year=' + year;
+    }
+    console.log(url, "AFTER");
+    fetch(url).then(function(response) { return response.json(); })
+        .then(function(data) {
+            const reviewList = document.getElementById('reviews-list');
+            reviewList.innerHTML = '';
+            data.forEach((m) => {
+                const tr = document.createElement('tr');
+                const td = document.createElement('td');
+                tr.appendChild(td).textContent = m.name;
+                const td2 = document.createElement('td');
+                tr.appendChild(td2).textContent = m.semester;
+                const td3 = document.createElement('td');
+                tr.appendChild(td3).textContent = m.year;
+                const td4 = document.createElement('td');
+                tr.appendChild(td4).textContent = m.review;
+                if (m.name !== null && m.year !== null && m.semester !== null && m.review !== null) {
+                    reviewList.appendChild(tr);
+                }
+            });
+        });
+
 }
 
 function main() {
